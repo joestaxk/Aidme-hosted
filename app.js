170571677b9e2b608cd9d73141fe86570ef5824c
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-21 03:22:22 +0100

    Updated with mongoose

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
+const mongoose = require('mongoose');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const mongoConnect = require('./utils/database').mongoConnect;
+// const mongoConnect = require('./utils/database').mongoConnect;
 
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
 
-mongoConnect(() => {
- // console.log(client);
+mongoose
+  .connect('mongodb+srv://Aidme:WaE1EO9dN4QlHCEP@aidme.crnapwb.mongodb.net/?retryWrites=true&w=majority')
+  .then(result => {
   app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+    console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+
   });
-});
+
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
-const getDb = require('../utils/database').getDb;
+//const getDb = require('../utils/database').getDb;
 
 exports.register = async (req, res, next) => {
   try {
@@ -15,8 +15,9 @@ exports.register = async (req, res, next) => {
       password
     } = req.body;
 
-    const db = getDb();
-    const user = await db.collection('users').findOne({ email });
+   // const db = getDb();
+    // const user = await db.collection('users').findOne({ email });
+    const user = await User.findOne({ email });
 
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
@@ -36,11 +37,11 @@ exports.register = async (req, res, next) => {
       confirmPassword: hashedPassword
     });
 
-    await db.collection('users').insertOne(createUser);
+    // await db.collection('users').insertOne(createUser);
+    await createUser.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
   }
 };
-
