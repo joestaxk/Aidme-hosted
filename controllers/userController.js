c083b79fe3b76067c74962b662e96e89ec164c2e
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-21 03:55:52 +0100

    Add files via upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,29 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 //const getDb = require('../utils/database').getDb;
 
 exports.register = async (req, res, next) => {
-  try {
+  await body('firstName').trim().not().isEmpty().withMessage('First name is required').run(req);
+  await body('lastName').trim().not().isEmpty().withMessage('Last name is required').run(req);
+  await body('email').isEmail().withMessage('Invalid email address').normalizeEmail().run(req);
+  await body('phoneNumber').optional().isMobilePhone().withMessage('Invalid phone number').run(req);
+  await body('address').optional().trim().not().isEmpty().withMessage('Address is required').run(req);
+  await body('country').optional().trim().not().isEmpty().withMessage('Country is required').run(req);
+  await body('gender').optional().isIn(['male', 'female', 'other']).withMessage('Invalid gender').run(req);
+  await body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long').run(req);
+  await body('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords do not match');
+    }
+    return true;
+  }).run(req);
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
     const {
       firstName,
       lastName,
@@ -40,8 +60,4 @@ exports.register = async (req, res, next) => {
     // await db.collection('users').insertOne(createUser);
     await createUser.save();
     res.status(201).json({ message: 'User created successfully' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Server error' });
-  }
 };
