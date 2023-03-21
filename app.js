f2fa49f0ba12d1b401cc1435f3925b7646825e18
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-21 02:38:46 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.log('Error connecting to MongoDB:', error);
-  });
+const mongoConnect = require('./utils/database').mongoConnect;
+
 
 app.use(bodyParser.json());
 app.use('/api/users', userRoutes);
 
-app.listen(port, () => {
+
+mongoConnect(() => {
+ // console.log(client);
+  app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+  });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const getDb = require('../utils/database').getDb;
 
 exports.register = async (req, res, next) => {
   try {
@@ -10,14 +12,17 @@ exports.register = async (req, res, next) => {
       address,
       country,
       gender,
-      password,
-      confirmPassword
+      password
     } = req.body;
 
-    const user = await User.findOne({ email });
+    const db = getDb();
+    const user = await db.collection('users').findOne({ email });
+
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const createUser = new User({
       firstName,
@@ -27,15 +32,15 @@ exports.register = async (req, res, next) => {
       address,
       country,
       gender,
-      password,
-      confirmPassword
+      password: hashedPassword,
+      confirmPassword: hashedPassword
     });
 
-    await createUser.save();
-
+    await db.collection('users').insertOne(createUser);
     res.status(201).json({ message: 'User created successfully' });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
   }
 };
+
