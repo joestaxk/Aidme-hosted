c35107e7aa04f55835c7461d4df7a0d0537bc06d
SteveParadox <39016370+SteveParadox@users.noreply.github.com>
2023-03-24 20:10:57 +0100

    Delete config directory

--- config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-   // MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost:27017/aidmeapp",
-    SECRET_KEY: process.env.SECRET_KEY || "3458056543656545676655463567685746453536786545346",
-  };
-  
