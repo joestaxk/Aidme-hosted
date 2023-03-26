c6d557155112d7a95ff02855bfe36870f35858c4
joestaxk <joestaxk@gmail.com>
2023-03-26 18:05:50 +0100

    Update: Added new mongoDB credentials for testing purposes

--- config/config.js
+++ config/config.js
@@ -11,7 +11,7 @@ const developmentMode = {
 
 const productionMode = {
    PORT: _.PORT,
-   DB_URI: `mongodb+srv://${_.DB_USER}:${_.DB_PASS}@a${_.DB_HOST}/?retryWrites=true&w=majority`
+   DB_URI: `mongodb+srv://${_.DB_USER}:${_.DB_PASS}@${_.DB_HOST}/?retryWrites=true&w=majority`
 }
 
 
