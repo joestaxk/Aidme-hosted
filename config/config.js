aef7f6f516e1a71d981f47d771ba6dec927a7857
joestaxk <joestaxk@gmail.com>
2023-03-30 12:51:08 +0100

    cors is valid to all incoming urls

--- config/config.js
+++ config/config.js
@@ -19,8 +19,9 @@ const productionMode = {
 }
 
 // smth here
+//validCors: [/^(http|https):\/\/(localhost:517+\d|127.0.0.1:517+\d)+$/],
 module.exports = Object.assign({
-    validCors: [/^(http|https):\/\/(localhost:517+\d|127.0.0.1:517+\d)+$/],
+    validCors: "*",
     mode:  _.NODE_ENV === "production" ? "Production" : "Development",
     JWT_SECRETKEY: _.JWT_SECRETKEY,
     JWT_EXPIRES_IN: _.JWT_EXPIRES_IN,
